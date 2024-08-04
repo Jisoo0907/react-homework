7e4babbdc49162c8f1c3c6dff47fd156c7b45293
jisoolee <szjslee@gmail.com>
2024-08-04 10:10:41 +0900

    init: vite-react 폴더 degit 후 환경 구성

---
+++ 08-html-vs-jsx-markup/src/main.jsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "/App";
+
+const domNode = document.getElementById("react-app");
+
+createRoot(domNode).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
