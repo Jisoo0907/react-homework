5226082aface3d55692bce619ea88fc5e0b174b9
jisoolee <szjslee@gmail.com>
2024-08-04 16:57:17 +0900

    feat: 컴포넌트 속성 검사

--- 120-type-check/src/components/Button.jsx
+++ 120-type-check/src/components/Button.jsx
@@ -1,21 +1,25 @@
-import { createRoot } from 'react-dom/client';
+import PropTypes from 'prop-types';
 
-function Button({ children, status = 'inactive' }) {
-  const statusMessage =
-    status === 'active' ? '회원가입 버튼 활성화' : '회원가입 버튼 비활성화';
-
-  const label = `${statusMessage}`;
+function Button({ text, disabled = true }) {
+  const statusMessage = disabled
+    ? '회원가입 버튼 비활성화'
+    : '회원가입 버튼 활성화';
 
   return (
     <button
-      className={`Button Button-${status}`}
-      aria-label={label}
+      className={`Button ${disabled ? 'Button-inactive' : 'Button-active'}`}
+      aria-label={statusMessage}
       type="button"
     >
-      {children}
+      {text}
     </button>
   );
 }
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+};
 
 const container = document.getElementById('react-app');
 
@@ -23,8 +27,8 @@ function ButtonListPage() {
   const buttonText = '동의하고 시작하기';
   return (
     <div role="group">
-      <Button status="active">{buttonText}</Button>
-      <Button status="inactive">{buttonText}</Button>
+      <Button text={buttonText} disabled={false} />
+      <Button text={buttonText} disabled={true} />
     </div>
   );
 }
