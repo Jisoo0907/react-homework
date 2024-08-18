ff4ea7ffc7c9ecc77a01e88261330a49179886a6
jisoolee <szjslee@gmail.com>
2024-08-18 22:54:20 +0900

    feat: select 컴포넌트 작성

--- 186-todo/src/components/Select/Select.jsx
+++ 186-todo/src/components/Select/Select.jsx
@@ -1,4 +1,11 @@
+import S from './Select.module.css';
+
 function Select() {
-  return <></>;
+  return (
+    <select name="time" id="time" className={S.component}>
+      <option value="morning">오전</option>
+      <option value="afternoon">오후</option>
+    </select>
+  );
 }
 export default Select;
